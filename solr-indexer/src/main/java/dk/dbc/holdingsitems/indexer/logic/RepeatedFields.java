029304a66e48d68bb093f442954cce0a1706799d
Morten Bøgeskov <mb@dbc.dk>
2018-03-13 10:58:57 +0100

    AGPL

--- solr-indexer/src/main/java/dk/dbc/holdingsitems/indexer/logic/RepeatedFields.java
+++ solr-indexer/src/main/java/dk/dbc/holdingsitems/indexer/logic/RepeatedFields.java
@@ -1,19 +1,19 @@
 /*
- * Copyright (C) 2017 DBC A/S (http://dbc.dk/)
+ * Copyright (C) 2017-2018 DBC A/S (http://dbc.dk/)
  *
- * This is part of dbc-holdings-items-solr-indexer
+ * This is part of holdings-items
  *
- * dbc-holdings-items-solr-indexer is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
+ * holdings-items is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * dbc-holdings-items-solr-indexer is distributed in the hope that it will be useful,
+ * holdings-items is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * GNU Affero General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
+ * You should have received a copy of the GNU Affero General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package dk.dbc.holdingsitems.indexer.logic;
--- update-webservice/src/main/java/dk/dbc/holdingsitems/update/AccessCache.java
+++ update-webservice/src/main/java/dk/dbc/holdingsitems/update/AccessCache.java
@@ -1,22 +1,20 @@
 /*
- * dbc-holdings-items-update-webservice
- * Copyright (C) 2015 Dansk Bibliotekscenter a/s, Tempovej 7-11, DK-2750 Ballerup,
- * Denmark. CVR: 15149043*
+ * Copyright (C) 2015-2018 DBC A/S (http://dbc.dk/)
  *
- * This file is part of dbc-holdings-items-update-webservice.
+ * This is part of holdings-items
  *
- * dbc-holdings-items-update-webservice is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as
- * published by the Free Software Foundation, either version 3 of the
- * License, or (at your option) any later version.
+ * holdings-items is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- * dbc-holdings-items-update-webservice is distributed in the hope that it will be useful,
+ * holdings-items is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with dbc-holdings-items-update-webservice.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 package dk.dbc.holdingsitems.update;
 
