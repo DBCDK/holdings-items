4b7c852d3897fed9e1043a64adfa63438cb3626e
Morten Bøgeskov <mb@dbc.dk>
2018-06-11 08:00:47 +0200

    Configure pg-queue-ee-diags via json, and use static name for jmx reporting

--- monitor/src/main/java/dk/dbc/holdingsitems/monitor/Status.java
+++ monitor/src/main/java/dk/dbc/holdingsitems/monitor/Status.java
@@ -19,20 +19,16 @@
 package dk.dbc.holdingsitems.monitor;
 
 import dk.dbc.holdingsitems.monitor.monitor.Timed;
-import dk.dbc.pgqueue.diags.QueueStatusBean;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Set;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import javax.annotation.Resource;
-import javax.ejb.EJB;
 import javax.ejb.Stateless;
 import javax.sql.DataSource;
-import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
-import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import org.slf4j.Logger;
@@ -48,41 +44,39 @@ public class Status {
 
     private static final Logger log = LoggerFactory.getLogger(Status.class);
 
-    private static final int DIAG_MAX_CACHE_AGE = 45;
-    private static final int DIAG_PERCENT_MATCH = 90;
-    private static final int DIAG_COLLAPSE_MAX_ROWS = 12500;
-
-    @Resource(lookup = Config.DATABASE)
+    @Resource(lookup = "jdbc/holdings-items")
     DataSource dataSource;
-
-    @EJB
-    QueueStatusBean queueStatus;
 
     @GET
     @Produces({MediaType.APPLICATION_JSON})
     @Timed
     public Response getStatus() {
         log.info("getStatus called ");
-        return Response.ok().entity("{ \"ok\": true }").build();
-    }
-
-    @GET
-    @Path("queue")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getQueueStatus(@QueryParam("ignore") String ignore) {
-        Set<String> ign = Collections.EMPTY_SET;
-        if(ignore != null && ! ignore.isEmpty()) {
-            ign = new HashSet<>(Arrays.asList(ignore.split(",")));
-            log.debug("getQueueStatus(ign = {})", ign);
+        try (Connection connection = dataSource.getConnection() ;
+             Statement stmt = connection.createStatement() ;
+             ResultSet resultSet = stmt.executeQuery("SELECT 1")) {
+            if (!resultSet.next()) {
+                throw new SQLException("No rows returned in `SELECT 1'");
+            }
+            return Response.ok().entity(new Resp()).build();
+        } catch (SQLException ex) {
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR).entity(new Resp(ex.getMessage())).build();
         }
-        return queueStatus.getQueueStatus(dataSource, DIAG_MAX_CACHE_AGE, DIAG_PERCENT_MATCH, DIAG_COLLAPSE_MAX_ROWS, ign);
     }
 
-    @GET
-    @Path("diags")
-    @Produces({MediaType.APPLICATION_JSON})
-    public Response getDiagDistribution(@QueryParam("zone") @DefaultValue("CET") String timeZoneName) {
-        return queueStatus.getDiagDistribution(timeZoneName, dataSource, DIAG_PERCENT_MATCH, DIAG_COLLAPSE_MAX_ROWS);
-    }
+    public static class Resp {
 
+        public boolean ok;
+        public String text;
+
+        public Resp() {
+            ok = true;
+            text = "Success";
+        }
+
+        public Resp(String diag) {
+            ok = false;
+            text = diag;
+        }
+    }
 }
