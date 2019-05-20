b89442f1cdf4fd958778da7d0affb7ae699b5be2
Morten Bøgeskov <mb@dbc.dk>
2019-05-20 11:18:54 +0200

    update-webservice status endpoint

---
+++ update-webservice/src/main/java/dk/dbc/holdingsitems/update/Status.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright (C) 2019 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of holdings-items-update-webservice
+ *
+ * holdings-items-update-webservice is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * holdings-items-update-webservice is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.holdingsitems.update;
+
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.List;
+import javax.annotation.Resource;
+import javax.ejb.Stateless;
+import javax.sql.DataSource;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ *
+ * @author DBC {@literal <dbc.dk>}
+ */
+@Stateless
+@Path("/status")
+public class Status {
+
+    private static final Logger log = LoggerFactory.getLogger(Status.class);
+
+    @Resource(lookup = C.DATASOURCE)
+    DataSource dataSource;
+
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response status() {
+        try (Connection connection = dataSource.getConnection() ;
+             Statement stmt = connection.createStatement() ;
+             ResultSet resultSet = stmt.executeQuery("SELECT 1")) {
+            if (resultSet.next())
+                return Response.ok().entity(new Resp()).build();
+            throw new SQLException("No rows in `SELECT 1`");
+
+        } catch (SQLException ex) {
+            log.error("Status SQL: {}", ex.getMessage());
+            log.debug("Status SQL: ", ex);
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                    .entity(new Resp("database-error")).build();
+        }
+    }
+
+    @SuppressFBWarnings("URF_UNREAD_PUBLIC_OR_PROTECTED_FIELD")
+    public static class Resp {
+
+        public boolean ok;
+        public String text;
+
+        public Resp() {
+            this.ok = true;
+            this.text = "Success";
+        }
+
+        public Resp(String diag) {
+            this.ok = false;
+            this.text = diag;
+        }
+    }
+
+}
