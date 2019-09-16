8fc1c7af5294ab4fb6d6d676626acc9d9fa495e5
Morten Bøgeskov <mb@dbc.dk>
2019-09-16 08:08:14 +0200

    Commandline DB migrate tool

---
+++ postgres-content/src/main/java/dk/dbc/holdingsitems/postgresql/MigrateMain.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2019 DBC A/S (http://dbc.dk/)
+ *
+ * This is part of holdings-items-postgresql-content
+ *
+ * holdings-items-postgresql-content is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * holdings-items-postgresql-content is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package dk.dbc.holdingsitems.postgresql;
+
+import dk.dbc.holdingsitems.DatabaseMigrator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import org.postgresql.ds.PGSimpleDataSource;
+
+/**
+ *
+ * @author Morten Bøgeskov (mb@dbc.dk)
+ */
+public class MigrateMain {
+
+    private static final Pattern DB_URL = Pattern.compile("^(?:postgres(?:ql)?)?(?:([^/@]+)(?::([^@]+))@)?([^:/]+)(?::([1-9][0-9]*))?/(\\w+)$");
+
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
+            System.err.println("Usage: [database-url]");
+            System.exit(1);
+        }
+        Matcher matcher = DB_URL.matcher(args[0]);
+        if (!matcher.matches()) {
+            System.err.println("Usage: [database-url]");
+            System.err.println("");
+            System.err.println("Not a database url");
+            System.exit(1);
+
+        }
+        String username = matcher.group(1);
+        System.out.println("username = " + username);
+        String password = matcher.group(2);
+        System.out.println("password = " + password);
+        String hostname = matcher.group(3);
+        System.out.println("hostname = " + hostname);
+        String port = matcher.group(4);
+        System.out.println("port = " + port);
+        String database = matcher.group(5);
+        System.out.println("database = " + database);
+
+        PGSimpleDataSource ds = new PGSimpleDataSource();
+        if (username != null)
+            ds.setUser(username);
+        if (password != null)
+            ds.setPassword(username);
+        ds.setServerName(hostname);
+        if (port != null)
+            ds.setPortNumber(Integer.parseUnsignedInt(port));
+        ds.setDatabaseName(database);
+        DatabaseMigrator.migrate(ds);
+    }
+}
