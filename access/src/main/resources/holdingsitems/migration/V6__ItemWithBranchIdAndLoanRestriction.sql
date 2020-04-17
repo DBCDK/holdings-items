0c69cb0456fddc24b7a0211e2795feabae71caf3
Morten Bøgeskov <mb@dbc.dk>
2020-04-17 08:55:14 +0200

    Default on branchId in database

--- access/src/main/resources/holdingsitems/migration/V6__ItemWithBranchIdAndLoanRestriction.sql
+++ access/src/main/resources/holdingsitems/migration/V6__ItemWithBranchIdAndLoanRestriction.sql
@@ -12,6 +12,6 @@ INSERT INTO item_loanRestriction (loanRestriction) VALUES ('e');
 INSERT INTO item_loanRestriction (loanRestriction) VALUES ('f');
 INSERT INTO item_loanRestriction (loanRestriction) VALUES ('g');
 
-ALTER TABLE item ADD COLUMN branchId TEXT NOT NULL;
-ALTER TABLE item ADD COLUMN loanRestriction VARCHAR(1) NOT NULL default '';
+ALTER TABLE item ADD COLUMN branchId TEXT NOT NULL DEFAULT '';
+ALTER TABLE item ADD COLUMN loanRestriction VARCHAR(1) NOT NULL DEFAULT '';
 ALTER TABLE item ADD CONSTRAINT item_loanRestriction_fk FOREIGN KEY (loanRestriction) REFERENCES item_loanRestriction (loanRestriction);
