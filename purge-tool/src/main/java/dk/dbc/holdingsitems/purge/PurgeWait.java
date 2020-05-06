6ed46190e2efd86dbf287fe9ce12eb21092f8d05
Morten Bøgeskov <mb@dbc.dk>
2020-05-06 07:11:28 +0200

    Removed purge from PurgeWait

--- purge-tool/src/main/java/dk/dbc/holdingsitems/purge/PurgeWait.java
+++ purge-tool/src/main/java/dk/dbc/holdingsitems/purge/PurgeWait.java
@@ -23,20 +23,13 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
  *
  */
 public class PurgeWait {
 
-    private static final Logger log = LoggerFactory.getLogger(PurgeWait.class);
-    private final String agencyName;
-    private final int agencyId;
     private final String trackingId;
-
-    private final boolean dryRun;
 
     private final Connection connection;
 
@@ -51,34 +44,12 @@ public class PurgeWait {
      */
     public PurgeWait(Connection connection, String agencyName, int agencyId, String trackingId, boolean dryRun) {
         this.connection = connection;
-        this.agencyName = agencyName;
-        this.agencyId = agencyId;
-        this.dryRun = dryRun;
         this.trackingId = trackingId;
     }
 
-
-    /**
-     * Commit or rollback for database
-     *
-     * @param count Count for log status
-     * @throws SQLException
-     */
-    private void commit(int count) throws SQLException {
-        if (dryRun) {
-            log.info("Rolled back at {}", count);
-            connection.rollback();
-        } else {
-            log.info("Commit at {}", count);
-            connection.commit();
-        }
-    }
-
     private static final String QUEUE_SIZE = "SELECT COUNT (*) FROM queue WHERE trackingid=?";
-    private static final String PURGE_ITEMS = "DELETE FROM holdingsitemsitem WHERE agencyId=?";
-    private static final String PURGE_COLLECTIONS = "DELETE FROM holdingsitemscollection WHERE agencyId=?";
 
-    public  void waitForQueue() throws SQLException, IOException {
+    public void waitForQueue() throws SQLException, IOException {
         System.out.println("Waiting for queue to be processed");
         int count = 1;
         while (true) {
@@ -92,18 +63,6 @@ public class PurgeWait {
                 }
             }
             if (count == 0) {
-                System.out.printf("Delete items and collections for %d: %s%n", agencyId, agencyName);
-                try (PreparedStatement deleteItems = connection.prepareStatement(PURGE_ITEMS) ;
-                     PreparedStatement deleteCollections = connection.prepareStatement(PURGE_COLLECTIONS);) {
-                    deleteItems.setInt(1, agencyId);
-                    int itemCount = deleteItems.executeUpdate();
-                    System.out.printf("%d items deleted from tables%n", itemCount);
-
-                    deleteCollections.setInt(1, agencyId);
-                    int collectionsCount = deleteCollections.executeUpdate();
-                    System.out.printf("%d collections, deleted from tables%n", collectionsCount);
-                    commit(collectionsCount);
-                }
                 return;
             } else {
                 System.out.println("Press enter to wait again.");
