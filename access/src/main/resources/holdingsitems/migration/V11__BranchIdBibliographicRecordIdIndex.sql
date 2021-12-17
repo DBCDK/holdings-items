4d28c5a71b1f72f093b0883c9c730bc17100bb91
Morten Bøgeskov <mb@dbc.dk>
2021-12-17 07:54:44 +0100

    Cleaner migrate script

--- access/src/main/resources/holdingsitems/migration/V11__BranchIdBibliographicRecordIdIndex.sql
+++ access/src/main/resources/holdingsitems/migration/V11__BranchIdBibliographicRecordIdIndex.sql
@@ -1,15 +1,1 @@
-DO
-$$
-DECLARE
-  has BOOLEAN;
-BEGIN
-    SELECT true INTO has FROM pg_indexes
-        WHERE tablename='item' AND
-              indexname='item_branchid_bibliographicrecordid';
-    IF has IS NULL THEN
-        -- Can be made `CONCURRENTLY` ahead of time
-        CREATE INDEX item_branchid_bibliographicrecordid ON item (branchid, bibliographicrecordid) WHERE branchid <> '';
-    END IF;
-END
-$$;
-
+CREATE INDEX IF NOT EXISTS item_branchid_bibliographicrecordid ON item (branchid, bibliographicrecordid) WHERE branchid <> '';
