5f3a4260c6279c58b53cde7be75aa419e6fdee20
Morten Bøgeskov <mb@dbc.dk>
2022-09-12 13:48:44 +0200

    pmd/spotbugs

--- access/src/main/java/dk/dbc/holdingsitems/jpa/BibliographicItemDetached.java
+++ access/src/main/java/dk/dbc/holdingsitems/jpa/BibliographicItemDetached.java
@@ -13,7 +13,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Objects;
 import java.util.Set;
-import java.util.stream.Collectors;
 import javax.persistence.EntityManager;
 
 import static dk.dbc.holdingsitems.jpa.BibliographicItemEntity.fromUnLocked;
