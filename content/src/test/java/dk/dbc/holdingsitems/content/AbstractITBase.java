309b4930bfd91eb816ff5bc527afb04ba13f4974
Morten Bøgeskov <mb@dbc.dk>
2022-10-14 09:35:37 +0200

    Integration test of lastLoanDate

---
+++ content/src/test/java/dk/dbc/holdingsitems/content/AbstractITBase.java
@@ -0,0 +1,46 @@
+package dk.dbc.holdingsitems.content;
+
+import com.github.dockerjava.api.model.ContainerNetwork;
+import dk.dbc.commons.testcontainers.postgres.DBCPostgreSQLContainer;
+import java.net.URI;
+import java.time.Duration;
+import javax.ws.rs.core.UriBuilder;
+import org.slf4j.LoggerFactory;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.output.Slf4jLogConsumer;
+import org.testcontainers.containers.wait.strategy.Wait;
+
+/**
+ *
+ * @author Morten Bøgeskov (mb@dbc.dk)
+ */
+public class AbstractITBase extends JpaBase {
+
+    private static final GenericContainer PAYARA = makeService(PG);
+    static final UriBuilder API_URI = UriBuilder.fromUri(URI.create("http://" + containerIp(PAYARA) + ":8080")).path("/api");
+
+    private static GenericContainer makeService(DBCPostgreSQLContainer pg) {
+        String dockerImagePostfix = System.getProperty("docker.image.postfix", "-current:latest");
+        GenericContainer container = new GenericContainer("holdings-items-content-service" + dockerImagePostfix)
+                .withLogConsumer(new Slf4jLogConsumer(LoggerFactory.getLogger("dk.dbc.PAYARA")))
+                .withEnv("JAVA_MAX_HEAP_SIZE", "1G")
+                .withEnv("HOLDINGS_ITEMS_POSTGRES_URL", pg.getPayaraDockerJdbcUrl())
+                .withEnv("LOG__dk_dbc", "DEBUG")
+                .withExposedPorts(8080)
+                .waitingFor(Wait.forHttp("/health"))
+                .withStartupTimeout(Duration.ofMinutes(3));
+        container.start();
+        return container;
+    }
+
+    private static String containerIp(GenericContainer container) {
+        return container.getCurrentContainerInfo()
+                .getNetworkSettings()
+                .getNetworks()
+                .values()
+                .stream()
+                .map(ContainerNetwork::getIpAddress)
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException("Container has no IP address?"));
+    }
+}
---
+++ content/src/test/java/dk/dbc/holdingsitems/content/ServiceIT.java
@@ -0,0 +1,40 @@
+package dk.dbc.holdingsitems.content;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import dk.dbc.holdingsitems.jpa.BibliographicItemEntity;
+import dk.dbc.holdingsitems.jpa.IssueEntity;
+import dk.dbc.holdingsitems.jpa.Status;
+import java.time.Instant;
+import java.time.LocalDate;
+import javax.ws.rs.core.Response;
+import org.junit.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.is;
+
+public class ServiceIT extends AbstractITBase {
+
+    @Test(timeout = 2_000L)
+    public void testLastLoanDateExistsInComplete() throws Exception {
+        System.out.println("testLastLoanDateExistsInComplete");
+
+        jpa(em -> {
+            BibliographicItemEntity bibliographicItemEntity = BibliographicItemEntity.from(em, 710100, "25912233", Instant.now(), LocalDate.now())
+                    .setTrackingId("x");
+
+            IssueEntity issueEntity = issueEntity(bibliographicItemEntity, "issue#1")
+                    .setIssueText("First Issue")
+                    .setReadyForLoan(1)
+                    .setTrackingId("x");
+
+            itemEntity(issueEntity, "a", Status.ON_SHELF);
+
+            bibliographicItemEntity.save();
+        });
+        JsonNode complete = get(API_URI.path("complete").path("710100").path("25912233").build())
+                .statusIs(Response.Status.OK)
+                .as(JsonNode.class);
+        assertThat(complete.isMissingNode(), is(false));
+    }
+
+}
