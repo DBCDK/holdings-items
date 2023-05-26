63c4123400bf6dc5c129d17238979fc8e2ef64e8
Morten Bøgeskov <mb@dbc.dk>
2023-05-26 08:11:53 +0200

    spotbugs/pmd/java-warnings

--- update-webservice/src/main/java/dk/dbc/holdingsitems/update/UpdateRequest.java
+++ update-webservice/src/main/java/dk/dbc/holdingsitems/update/UpdateRequest.java
@@ -50,7 +50,6 @@ import java.util.Optional;
 import java.util.Set;
 import java.util.function.Consumer;
 import java.util.function.Supplier;
-import org.checkerframework.checker.lock.qual.Holding;
 import org.eclipse.microprofile.metrics.Timer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
