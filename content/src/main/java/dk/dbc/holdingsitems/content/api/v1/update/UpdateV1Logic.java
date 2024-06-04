695c6f7f64fecf6c17517981779aba64cc928b63
Morten Bøgeskov <mb@dbc.dk>
2024-06-04 14:00:39 +0200

    First accession date for new records

--- content/src/main/java/dk/dbc/holdingsitems/content/api/v1/update/UpdateV1Logic.java
+++ content/src/main/java/dk/dbc/holdingsitems/content/api/v1/update/UpdateV1Logic.java
@@ -88,6 +88,7 @@ public class UpdateV1Logic {
                                                                                   Instant.EPOCH, LocalDate.EPOCH);
                     if (entity.isNew()) {
                         entity.setTrackingId("");
+                        entity.setFirstAccessionDate(LocalDate.now());
                         entity.save();
                     }
                 });
